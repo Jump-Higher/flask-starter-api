62a7e6a07d269dbf4b68e89dc3a1226f42db9415
Mochamad sandhyka <moch.sandhyka@gmail.com>
2023-07-25 19:28:17 +0700

    deactivate mail

--- app/controllers/user.py
+++ app/controllers/user.py
@@ -74,7 +74,7 @@ def create_user():
         id_user = uuid4()
 
         sendMail = sendEmail(json_body['email'],f"Activate Your Account here : {os.getenv('URL_ACTIVATE_USER')}{id_user}","Activate Your Account")
-        mail.send(sendMail)
+        #mail.send(sendMail)
 
         # add to tbl_user
         new_user = User(id_user = id_user, 
